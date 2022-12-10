f5cec70e625422108d2ad279e4af773b75f0a680
faizanjavaid <44579971+faizanjavaid@users.noreply.github.com>
2022-12-11 00:52:32 +0500

    update

--- user/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ user/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Software\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Software\GithubRepositry\taxi\user"
+export "FLUTTER_ROOT=/Users/faizamjavaid/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Applications/XAMPP/xamppfiles/htdocs/taxi/user"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
